fe11a7258855ac721af875a4c4b8b9c341ddd047
Rithusri07 <rithusrisundar@gmail.com>
2025-03-06 12:19:45 +0530

    Add files via upload

---
+++ src/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+export default function Login({ onLogin }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Login</div>
+      </div>
+      <form onSubmit={handleLogin}>
+        <center>
+          <div className="forms">
+            <table className="table">
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="col">
+                      <input
+                        className="input"
+                        type="email"
+                        placeholder="📧 Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="col">
+                      <input
+                        className="input"
+                        type="password"
+                        placeholder="🔒 Enter password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="error"></div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </center>
+        <div className="submit-con">
+          <button id="login" type="submit" className="submit">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
